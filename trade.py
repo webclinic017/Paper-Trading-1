91d49ac8ba9dbde11a5ccce62080b7caab075c6e
darren4 <darren.tas7@gmail.com>
2022-01-01 17:20:22 -0500

    simple moving average trading

--- trade.py
+++ trade.py
@@ -9,14 +9,52 @@ from alpaca_trade_api.rest import TimeFrame
 class LongSimple:
 	def __init__(self):
 		self.alpaca = tradeapi.REST(API_KEY, SECRET_KEY, APCA_API_BASE_URL, 'v2')
+		self.trades = []
+		self.holding = False
+		self.ticker = 'AAPL'
+
+	def get_mov_avg(self, ticker):
+		barset = self.alpaca.get_barset(ticker, '5Min')
+		total = 0
+		for entity in barset:
+			total += entity['o']
+		return total / len(barset)
 
 	def run(self):
-		# Cancel existing orders
-		orders = self.alpaca.list_orders(status="open")
-		for order in orders:
-			self.alpaca.cancel_order(order.id)
-		print("Waiting for the market to open")
-		
+		while True:
+			clock = self.alpaca.get_clock()
+			if clock.is_open:
+				barset = self.alpaca.get_barset(self.ticker, 'minute', limit=1)
+				price = barset[0].o
+				if price <= self.get_mov_avg(self.ticker):
+					if not self.holding:
+						self.alpaca.submit_order(
+							symbol=self.ticker,
+							qty=6,
+							side='buy',
+							type='market',
+							time_in_force='gtc'
+						)
+						self.holding = True
+				else:
+					if self.holding:
+						self.alpaca.submit_order(
+							symbol=self.ticker,
+							qty=6,
+							side='sell',
+							type='market',
+							time_in_force='gtc'
+						)
+						self.holding = False
+			else:
+				# Cancel existing orders
+				orders = self.alpaca.list_orders(status="open")
+				for order in orders:
+					self.alpaca.cancel_order(order.id)
+				print("Waiting for the market to open")
+			
+			# wait a bit
+			time.sleep(5)
 		
 
 
